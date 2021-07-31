9761cfc1423947d3f117327585e271a454554b00
Vishwarrior26 <srimathkandali@gmail.com>
2021-07-31 15:21:06 -0700

    Maintained case sensitivity

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ tweet = input("What is your tweet?\n")
 
 
 def censor_tweet(tweet):
+    originaltweet = tweet.split()
     tweet = re.sub(r'[^\w\s]', '', tweet)
     tweet_list = tweet.lower().split()
     for i in range(len(tweet_list)):
+        print("In for loop")
         tweet_array = [tweet_list[i]]
         if predict(tweet_array) == 1:
-            tweet_list[i] = '*' * len(tweet_list[i])
-    censor_string = ' '.join(tweet_list)
+            originaltweet[i] = '*' * len(originaltweet[i])
+    censor_string = ' '.join(originaltweet)
     return censor_string
 
 
