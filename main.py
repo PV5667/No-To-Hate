79de0b4d642f4b0444b94884021e12613dec9b9c
Vishwarrior26 <67435125+Vishwarrior26@users.noreply.github.com>
2021-07-31 14:29:22 -0700

    Changed badlist source

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import re
+from profanity_check import predict, predict_prob
 
-badlist = ['bitch', 'fuck','crap','asshole']
 tweet = input("What is your tweet?\n")
 
-def censor_tweet(tweet):
-  tweet = re.sub(r'[^\w\s]', '', tweet)
-  tweet_list = tweet.lower().split()
-  #print(tweet_list)
-  #print(badlist)
-  for i in range(len(tweet_list)):
-    for j in range(len(badlist)):
-      if tweet_list[i] == badlist[j]:
-        badword = tweet_list[i]
-        censor = '*'*len(badword)
-        #print(censor)
-        tweet_list[i] = censor
-        #print(tweet_list)
-  censor_string = ' '.join(tweet_list)
-  return censor_string
 
-print(censor_tweet(tweet))
+def censor_tweet(tweet):
+    tweet = re.sub(r'[^\w\s]', '', tweet)
+    tweet_list = tweet.lower().split()
+    for i in range(len(tweet_list)):
+        tweet_array = [tweet_list[i]]
+        if predict(tweet_array) == 1:
+            tweet_list[i] = '*' * len(tweet_list[i])
+    censor_string = ' '.join(tweet_list)
+    return censor_string
+
+
+# print(censor_tweet(tweet))
