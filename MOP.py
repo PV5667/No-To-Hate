d57ed08b4eca685f49e43f5fc21e4bf622f0ecd9
Vishwarrior26 <srimathkandali@gmail.com>
2021-07-31 23:38:20 -0700

    Commit

--- MOP.py
+++ MOP.py
@@ -16,7 +16,7 @@ CHARS_MAPPING = {
     "s": ("s", "$"),
     "t": ("t", "7", "%"),
 }
-num = 10000
+num = 100000
 
 
 def file():
@@ -24,33 +24,32 @@ def file():
         badlist = pickle.load(fp)
 
 
-openfileT = timeit.Timer(file).timeit(number=num) / num
-
-
-for i in range(len(badlist)):
-    if len(badlist[i]) < 20:
-        combos = [
-            (char,
-             ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
-            for char in iter(badlist[i])
-        ]
-        leet = ["".join(pattern) for pattern in product(*combos)]
-        for j in range(len(leet)):
-            badlist.append(leet[j])
-
-# input("What is your tweet?\n")
-tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1']]"
-
-
-def censor(tweet):
-    originaltweet = tweet.split()
-    tweet = re.sub(r'[^\w\s]', '', tweet)
-    tweet_list = tweet.lower().split()
-    for i in range(len(tweet_list)):
-        try:
-            temp = badlist.index(tweet_list[i])
-            originaltweet[i] = '*' * len(originaltweet[i])
-        except:
-            pass
-    censor_string = ' '.join(originaltweet)
-    return censor_string
+print(timeit.Timer(file).timeit(number=num) / num)
+#
+# for i in range(len(badlist)):
+#     if len(badlist[i]) < 20:
+#         combos = [
+#             (char,
+#              ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
+#             for char in iter(badlist[i])
+#         ]
+#         leet = ["".join(pattern) for pattern in product(*combos)]
+#         for j in range(len(leet)):
+#             badlist.append(leet[j])
+#
+# # input("What is your tweet?\n")
+# tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1']]"
+#
+#
+# def censor(tweet):
+#     originaltweet = tweet.split()
+#     tweet = re.sub(r'[^\w\s]', '', tweet)
+#     tweet_list = tweet.lower().split()
+#     for i in range(len(tweet_list)):
+#         try:
+#             temp = badlist.index(tweet_list[i])
+#             originaltweet[i] = '*' * len(originaltweet[i])
+#         except:
+#             pass
+#     censor_string = ' '.join(originaltweet)
+#     return censor_string
