4c5b4472bc1815e181f833fb7248d01125789d8e
Vishwarrior26 <srimathkandali@gmail.com>
2021-07-31 23:29:43 -0700

    Commit message

--- MOJ.py
+++ MOJ.py
@@ -17,31 +17,31 @@ CHARS_MAPPING = {
     "t": ("t", "7", "%"),
 }
 num = 10000
+
+
 def file():
-    global badlist
     with open('data - Copy.json', 'r') as f:
         badlist = json.load(f)
 
-openfileT = timeit.Timer(file).timeit(number=num)/num
 
-def permute():
-    for i in range(len(badlist)):
-        if len(badlist[i]) < 20:
-            combos = [
-                (char,
-                 ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
-                for char in iter(badlist[i])
-            ]
-            leet = ["".join(pattern) for pattern in product(*combos)]
-            for j in range(len(leet)):
-                badlist.append(leet[j])
-permuteT = timeit.Timer(permute).timeit(number=num)/num
+openfileT = timeit.Timer(file).timeit(number=num) / num
+
+for i in range(len(badlist)):
+    if len(badlist[i]) < 20:
+        combos = [
+            (char,
+             ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
+            for char in iter(badlist[i])
+        ]
+        leet = ["".join(pattern) for pattern in product(*combos)]
+        for j in range(len(leet)):
+            badlist.append(leet[j])
 
 # input("What is your tweet?\n")
 tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1']]"
 
 
-def censor(tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1']]"):
+def censor(tweet):
     originaltweet = tweet.split()
     tweet = re.sub(r'[^\w\s]', '', tweet)
     tweet_list = tweet.lower().split()
@@ -53,5 +53,3 @@ def censor(tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1'
             pass
     censor_string = ' '.join(originaltweet)
     return censor_string
-censorT = timeit.Timer(censor).timeit(number=num)/num
-#print(censor_tweet_bad_list(tweet))
--- MOP.py
+++ MOP.py
@@ -10,37 +10,39 @@ CHARS_MAPPING = {
     "o": ("o", "*", "0",),
     "u": ("u", "*", "v"),
     "v": ("v", "*", "u"),
-    "l": ("l", "1"),
-    "h": ("h", "#"),
+    "l": ("1"),
+    "h": ("#"),
     "e": ("e", "*"),
     "s": ("s", "$"),
     "t": ("t", "7", "%"),
 }
+num = 10000
 
-@profile
-def open_file():
 
+def file():
     with open('outfile - Copy', 'rb') as fp:
         badlist = pickle.load(fp)
 
-@profile
-def permute():
-    for i in range(len(badlist)):
-        if len(badlist[i]) < 20:
-            combos = [
-                (char,
-                 ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
-                for char in iter(badlist[i])
-            ]
-            leet = ["".join(pattern) for pattern in product(*combos)]
-            for j in range(len(leet)):
-                badlist.append(leet[j])
+
+openfileT = timeit.Timer(file).timeit(number=num) / num
+
+
+for i in range(len(badlist)):
+    if len(badlist[i]) < 20:
+        combos = [
+            (char,
+             ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
+            for char in iter(badlist[i])
+        ]
+        leet = ["".join(pattern) for pattern in product(*combos)]
+        for j in range(len(leet)):
+            badlist.append(leet[j])
 
 # input("What is your tweet?\n")
 tweet = "TAWOHTOAHFHOAynr921yt-er 12974t19274t [oquhfd y9w7etr b9['1']]"
 
-@profile
-def censor_tweet_bad_list(tweet):
+
+def censor(tweet):
     originaltweet = tweet.split()
     tweet = re.sub(r'[^\w\s]', '', tweet)
     tweet_list = tweet.lower().split()
@@ -52,6 +54,3 @@ def censor_tweet_bad_list(tweet):
             pass
     censor_string = ' '.join(originaltweet)
     return censor_string
-
-
-print(censor_tweet_bad_list(tweet))
