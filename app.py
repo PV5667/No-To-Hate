97d5610df16e20f3c8af59ea037b85b8e79d476a
Pranav <reachpv05@gmail.com>
2021-08-01 02:02:29 -0700

    Got ML model working on our platform.

--- app.py
+++ app.py
@@ -1,21 +1,122 @@
-from flask import Flask, flash, jsonify, render_template, request
+from flask import Flask, flash, jsonify, render_template, request, redirect, url_for
 from flask.templating import render_template
 import tensorflow as tf
 import numpy as np
-
+import os
+import csv
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import sklearn.preprocessing
+from sklearn.preprocessing import LabelBinarizer
+import re
+import pickle
+from itertools import product
 
 app = Flask(__name__)
+############################################################################################################
+CHARS_MAPPING = {
+    "a": ("a", "*", "@"),
+    "i": ("i", "*", "l", "1", "!"),
+    "o": ("o", "*", "0",),
+    "u": ("u", "*", "v"),
+    "v": ("v", "*", "u"),
+    "l": ("1"),
+    "h": ("#"),
+    "e": ("e", "*"),
+    "s": ("s", "$"),
+    "t": ("t", "7", "%"),
+}
+
+with open('outfile', 'rb') as fp:
+    badlist = pickle.load(fp)
 
 
-def run_through_model(tweet):
+for i in range(len(badlist)):
+    if len(badlist[i]) < 20:
+        combos = [
+            (char,
+             ) if char not in CHARS_MAPPING else CHARS_MAPPING[char]
+            for char in iter(badlist[i])
+        ]
+        leet = ["".join(pattern) for pattern in product(*combos)]
+        for j in range(len(leet)):
+            badlist.append(leet[j])
 
-    model = tf.keras.models.load_model('weights1.h5')
-    prediction = model.predict(np.array([tweet]))
-    for i in prediction:
-        labels = ['hate', 'normal']
-        return labels[np.argmax(i)]
+
+def censor(tweet):
+    originaltweet = tweet.split()
+    tweet = re.sub(r'[^\w\s]', '', tweet)
+    tweet_list = tweet.lower().split()
+    for i in range(len(tweet_list)):
+        try:
+            temp = badlist.index(tweet_list[i])
+            originaltweet[i] = '*' * len(originaltweet[i])
+        except:
+            pass
+    censor_string = ' '.join(originaltweet)
+    return censor_string
+############################################################################################################################################################
+def prep_for_model(tweet):
+    vocab_size = 10000
+    embedding_dim = 16
+    max_length = 100
+    trunc_type='post'
+    padding_type='post'
+    oov_tok = "<OOV>"
+    training_size = 20000
+
+    with open('train_tweets.txt') as file:
+        train_tweets = file.readlines()
+        file.close()
+    with open('test_tweets.txt') as file:
+        test_tweets = file.readlines()
+        file.close()
+    with open('train_labels.txt') as file:
+        train_labels = file.readlines()
+        file.close()
+    with open('test_labels.txt') as file:
+        test_labels = file.readlines()
+        file.close()
+    print(len(train_tweets))
+    tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+    tokenizer.fit_on_texts(train_tweets)
+
+    word_index = tokenizer.word_index
+
+    train_sequences = tokenizer.texts_to_sequences(train_tweets)
+    train_padded = pad_sequences(train_sequences, maxlen = max_length, 
+                             padding = padding_type, 
+                             truncating = trunc_type)
 
 
+    test_sequences = tokenizer.texts_to_sequences(test_tweets)
+    test_padded = pad_sequences(test_sequences, maxlen=max_length,
+                            padding=padding_type,
+                            truncating=trunc_type)
+
+    train_padded = np.array(train_padded)
+    train_labels = np.array(train_labels)
+    test_padded = np.array(test_padded)
+    test_labels = np.array(test_labels)
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(24, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+        ])
+    tweets = [tweet] 
+    sequences = tokenizer.texts_to_sequences(tweets)
+    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+    model.load_weights("model.h5")
+    prediction = model.predict(padded)
+    print(prediction)
+    return prediction
+
+
+###################################################################################################################################################################
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -32,9 +133,23 @@ def real_world():
 def handling():
     tweet = request.form['tweet']
     user_name = request.form['user_name']
-    censored_string = censor_tweet_bad_list(tweet)
-    #model_prediction = run_through_model(tweet)
-    return render_template("handling.html", user_name=user_name, censored_string=censored_string, model_prediction  = 5)
+    censored_string = censor(tweet)
+    model_prediction = prep_for_model(tweet)
+    threshold = 0.90
+    if model_prediction >= threshold:
+        cleared = True
+    else:
+        cleared = False
+    return render_template("handling.html", user_name=user_name, censored_string=censored_string, model_prediction  = model_prediction, cleared = cleared)
 
+
+
+@app.route("/censored")
+def censored():
+    return 5
+
+@app.route("/clear")
+def clear():
+    return 6
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ machine_learning.py
@@ -0,0 +1,172 @@
+import os
+import csv
+import tensorflow
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import sklearn.preprocessing
+from sklearn.preprocessing import LabelBinarizer
+import numpy as np
+import tensorflow as tf
+
+
+data = []
+test_filenames = []
+train_filenames = []
+labels = []
+count = 0 
+train_count = 0
+test_count = 0
+
+annotations_filename = "hate-speech-dataset/annotations_metadata.csv"
+
+for file in os.listdir("hate-speech-dataset/sampled_train"):
+  train_file_name = file[:len(file)-4]
+  train_filenames.append(train_file_name)
+
+for file in os.listdir('hate-speech-dataset/sampled_test'):
+  test_file_name = file[:len(file)-4]
+  test_filenames.append(test_file_name)
+
+for file_name in train_filenames:
+  with open(annotations_filename, 'r') as csvfile:
+      datareader = csv.reader(csvfile)
+      for row in datareader:
+        if row[0] == file_name:
+          labels.append(row[4])
+          count +=1
+
+for file_name in test_filenames:
+  with open(annotations_filename, 'r') as csvfile:
+      datareader = csv.reader(csvfile)
+      for row in datareader:
+        if row[0] == file_name:
+          labels.append(row[4])
+          count +=1
+
+
+for file_name in train_filenames:
+  with open(f"hate-speech-dataset/sampled_train/{file_name}.txt") as file:
+    line = file.read()
+    data.append(line)
+    file.close()
+    train_count +=1
+
+for file_name in test_filenames:
+  with open(f"hate-speech-dataset/sampled_test/{file_name}.txt") as file:
+    line = file.read()
+    data.append(line)
+    file.close()
+    test_count +=1
+
+print("count",count)
+print("train_count",train_count)
+print("test_count",test_count)
+
+
+
+
+
+lb = LabelBinarizer()
+labels = lb.fit_transform(labels)
+print(lb.classes_)
+# 0 = hate 1 = noHate
+# changing labels to 1's and 0's
+
+
+tokenizer = Tokenizer(oov_token='<OOV>')
+tokenizer.fit_on_texts(data)
+word_index = tokenizer.word_index
+
+
+
+sequences = tokenizer.texts_to_sequences(data)
+padded = pad_sequences(sequences, padding='post')
+
+
+training_size = 2152
+
+train_tweets = data[0:training_size]
+with open('train_tweets.txt', 'w') as f:
+    for item in train_tweets:
+        f.write("%s\n" % item)
+
+test_tweets = data[training_size:]
+with open('test_tweets.txt', 'w') as f:
+    for item in test_tweets:
+        f.write("%s\n" % item)
+
+train_labels = labels[0:training_size]
+
+with open('train_labels.txt', 'w') as f:
+    for item in train_labels:
+        f.write("%s\n" % item)
+
+
+test_labels = labels[training_size:]
+
+with open('test_labels.txt', 'w') as f:
+    for item in test_labels:
+        f.write("%s\n" % item)
+
+print(len(train_tweets))
+print(len(test_tweets))
+
+print(len(train_labels))
+print(len(test_labels))
+
+
+
+vocab_size = 10000
+embedding_dim = 16
+max_length = 100
+trunc_type='post'
+padding_type='post'
+oov_tok = "<OOV>"
+training_size = 20000
+
+tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+tokenizer.fit_on_texts(train_tweets)
+
+
+
+word_index = tokenizer.word_index
+
+
+
+
+train_sequences = tokenizer.texts_to_sequences(train_tweets)
+train_padded = pad_sequences(train_sequences, maxlen = max_length, 
+                             padding = padding_type, 
+                             truncating = trunc_type)
+
+
+test_sequences = tokenizer.texts_to_sequences(test_tweets)
+test_padded = pad_sequences(test_sequences, maxlen=max_length,
+                            padding=padding_type,
+                            truncating=trunc_type)
+
+
+
+
+train_padded = np.array(train_padded)
+train_labels = np.array(train_labels)
+test_padded = np.array(test_padded)
+test_labels = np.array(test_labels)
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(24, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+
+
+sentence = ["You are very nice, I love you, you're so lovely, i love you", "You are so bad", "You are so freaking stupid at that you stupid little stupid destruction", 
+            "so freaking stupid stupid little stupid destruction", "idiot idiot idiot", "The weather is fine today"]
+sequences = tokenizer.texts_to_sequences(sentence)
+padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+
+model.load_weights("model.h5")
+print(model.predict(padded))
