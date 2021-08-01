437ae8762365fd4568caf5dbad54d7cf19313b26
Vishwarrior26 <srimathkandali@gmail.com>
2021-08-01 09:03:57 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ app = Flask(__name__)
 with open('badlist', 'rb') as fp:
     badlist = pickle.load(fp)
 
+
 def censor(tweet):
     originaltweet = tweet.split()
     tweet_list = tweet.lower().split()
@@ -29,12 +30,14 @@ def censor(tweet):
     return censor_string
 
 ############################################################################################################################################################
+
+
 def prep_for_model(tweet):
     vocab_size = 10000
     embedding_dim = 16
     max_length = 100
-    trunc_type='post'
-    padding_type='post'
+    trunc_type = 'post'
+    padding_type = 'post'
     oov_tok = "<OOV>"
     training_size = 20000
 
@@ -51,21 +54,20 @@ def prep_for_model(tweet):
         test_labels = file.readlines()
         file.close()
     print(len(train_tweets))
-    tokenizer = Tokenizer(num_words = vocab_size, oov_token=oov_tok)
+    tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
     tokenizer.fit_on_texts(train_tweets)
 
     word_index = tokenizer.word_index
 
     train_sequences = tokenizer.texts_to_sequences(train_tweets)
-    train_padded = pad_sequences(train_sequences, maxlen = max_length,
-                             padding = padding_type,
-                             truncating = trunc_type)
-
+    train_padded = pad_sequences(train_sequences, maxlen=max_length,
+                                 padding=padding_type,
+                                 truncating=trunc_type)
 
     test_sequences = tokenizer.texts_to_sequences(test_tweets)
     test_padded = pad_sequences(test_sequences, maxlen=max_length,
-                            padding=padding_type,
-                            truncating=trunc_type)
+                                padding=padding_type,
+                                truncating=trunc_type)
 
     train_padded = np.array(train_padded)
     train_labels = np.array(train_labels)
@@ -73,15 +75,17 @@ def prep_for_model(tweet):
     test_labels = np.array(test_labels)
 
     model = tf.keras.Sequential([
-    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
-    tf.keras.layers.GlobalAveragePooling1D(),
-    tf.keras.layers.Dense(20, activation='relu'),
-    tf.keras.layers.Dropout(0.5),
-    tf.keras.layers.Dense(1, activation='sigmoid')
+        tf.keras.layers.Embedding(
+            vocab_size, embedding_dim, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(20, activation='relu'),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1, activation='sigmoid')
     ])
     tweets = [tweet]
     sequences = tokenizer.texts_to_sequences(tweets)
-    padded = pad_sequences(sequences, maxlen=max_length, padding=padding_type, truncating=trunc_type)
+    padded = pad_sequences(sequences, maxlen=max_length,
+                           padding=padding_type, truncating=trunc_type)
 
     model.load_weights("model.h5")
     prediction = model.predict(padded)
@@ -94,13 +98,16 @@ def prep_for_model(tweet):
 def home():
     return render_template("index.html")
 
+
 @app.route("/black-list")
 def black_list():
     return render_template("BlackList.html")
 
+
 @app.route("/real-world")
 def real_world():
     return render_template("RealWorld.html")
+
 
 @app.route("/handling", methods=['POST'])
 def handling():
@@ -113,16 +120,18 @@ def handling():
         cleared = True
     else:
         cleared = False
-    return render_template("handling.html", user_name=user_name, censored_string=censored_string, model_prediction  = model_prediction, cleared = cleared)
-
+    return render_template("handling.html", user_name=user_name, censored_string=censored_string, model_prediction=model_prediction, cleared=cleared)
 
 
 @app.route("/censored")
 def censored():
     return 5
 
+
 @app.route("/clear")
 def clear():
     return 6
+
+
 if __name__ == "__main__":
     app.run(debug=True)
