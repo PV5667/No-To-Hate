30caa731af7fd538c11072293e10a60bff586307
Pranav <reachpv05@gmail.com>
2021-07-31 20:00:12 -0700

    added model predicting functionality(prototype), edited handling page

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from flask.templating import render_template
 import re
 from profanity_check import predict, predict_prob
 import pickle
+import tensorflow as tf
+import numpy as np
+
 
 app = Flask(__name__)
 
@@ -23,6 +26,15 @@ def censor_tweet_bad_list(tweet):
     censor_string = ' '.join(originaltweet)
     return censor_string
 
+def run_through_model(tweet):
+
+    model = tf.keras.models.load_model('weights1.h5')
+    prediction = model.predict(np.array([tweet]))
+    for i in prediction:
+        labels = ['hate', 'normal']
+        return labels[np.argmax(i)]
+
+
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -40,7 +52,8 @@ def handling():
     tweet = request.form['tweet']
     user_name = request.form['user_name']
     censored_string = censor_tweet_bad_list(tweet)
-    return f"<h1>Hi {user_name}, your censored tweet is: {censored_string} <h1>"
+    model_prediction = run_through_model(tweet)
+    return render_template("handling.html", user_name=user_name, censored_string=censored_string, model_prediction = model_prediction)
 
 if __name__ == "__main__":
     app.run(debug=True)
