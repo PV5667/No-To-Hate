7ca89ca780d52c567f23f71b540b608e0d162f63
Pranav <reachpv05@gmail.com>
2021-07-31 17:25:45 -0700

    Added censoring functionality to homepage form

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from flask import Flask, flash, jsonify, render_template, request
 from flask.templating import render_template
+import re
+from profanity_check import predict, predict_prob
+import pickle
 
 app = Flask(__name__)
 
+with open('outfile', 'rb') as fp:
+    badlist = pickle.load(fp)
+
+
+def censor_tweet_bad_list(tweet):
+    originaltweet = tweet.split()
+    tweet = re.sub(r'[^\w\s]', '', tweet)
+    tweet_list = tweet.lower().split()
+    for i in range(len(tweet_list)):
+        try:
+            temp = badlist.index(tweet_list[i])
+            originaltweet[i] = '*' * len(originaltweet[i])
+        except:
+            pass
+    censor_string = ' '.join(originaltweet)
+    return censor_string
 
 @app.route("/")
 def home():
@@ -20,8 +39,14 @@ def real_world():
 def handling():
     tweet = request.form['tweet']
     user_name = request.form['user_name']
-    return f"<h1>Hi {user_name}, your tweet was: {tweet} <h1>"
+    censored_string = censor_tweet_bad_list(tweet)
+    return f"<h1>Hi {user_name}, your censored tweet is: {censored_string} <h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
 
